dfa87f557e30d6c756927502d49eea62f79ccb20
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2023-04-28 06:59:02 -0500

    Display submission in toast
    Generate mock submission data, add current submission to state, and display submission in toast. After the toast closes, the state is cleared and the toast is closed.

--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,88 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Snackbar from '@mui/material/Snackbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import CloseIcon from '@mui/icons-material/Close';
+import { createMockFormSubmission, saveLikedFormSubmission } from './service/mockServer';
+import Chance from "chance"
 
-import { createMockFormSubmission } from './service/mockServer';
+
 
 export default function Header() {
+  const chance = Chance();
+  const [currentSubmission, setCurrentSubmission] = useState(null);
+  const [toastStatus, setToastStatus] = useState(false);
+
+  // Set the current submission in state and open toast with the message
+  const handleSubmit = async () => {
+    let submission = {
+      id: chance.guid(),
+      data: {
+        email: chance.email(),
+        firstName: chance.first(),
+        lastName: chance.last(),
+        liked: false,
+      },
+    };
+    setCurrentSubmission(submission);
+    setToastStatus(true)
+  };
+
+  // Handle toast close and clear current submission
+  const handleClose = () => {
+    setToastStatus(false);
+    setCurrentSubmission(null);
+  }
+
+  const action = (
+    <React.Fragment>
+      <Button>Like</Button>
+      <IconButton onClick={handleClose}>
+        <CloseIcon fontSize="small" sx={{ color: "#fff" }} />
+      </IconButton>
+    </React.Fragment>
+  )
+
   return (
-    <Box sx={{flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
+      <Snackbar
+        open={toastStatus}
+        autoHideDuration={10000}
+        onClose={handleClose}
+        message={currentSubmission != null && (
+          <Box>
+            <Typography vairant="body2">{currentSubmission.data.firstName} {currentSubmission.data.lastName}</Typography>
+            <Typography vairant="body2">{currentSubmission.data.email}</Typography>
+          </Box>
+        )}
+        action={action}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      />
       <AppBar position="static">
         <Toolbar>
           <IconButton
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{marginRight: 2}}
+            sx={{ marginRight: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" sx={{flexGrow: 1}}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Toast Exercise
           </Typography>
           <Button
             variant="contained"
             size="small"
             color="secondary"
-            onClick={() => createMockFormSubmission()}
+            onClick={() => handleSubmit()}
           >
             New Submission
           </Button>
